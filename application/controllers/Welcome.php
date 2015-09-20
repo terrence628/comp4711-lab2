1fbe8d1af5f88192541ec42b465d03e4ed2608cd
terrence628 <terrence_lam@hotmail.com>
2015-09-20 16:41:15 -0700

    basic 3 removed css, img and data placed in views

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -21,6 +21,27 @@ class Welcome extends Application {
 	public function index()
 	{
             //$this->load->view('welcome');
+            
+             // get the newest images from our model
+            $pix = $this->images->newest();
+            
+            // build an array of formatted cells for them
+            foreach($pix as $picture)
+                $cells[] = $this->parser->parse('_cell', (array)$picture, true);
+            
+            // prime the table class
+            $this->load->library('table');
+            $parms = array(
+                'table_open' => '<table class="gallery">',
+                'cell_start' => '<td class="oneimage">',
+                'cell_alt_start' => '<td class="oneimage">'
+            );
+            $this->table->set_template($parms);
+            
+            // finally! generate the table
+            $rows = $this->table->make_columns($cells, 3);
+            $this->data['thetable'] = $this->table->generate($rows);
+            
             $this->data['pagebody'] = 'welcome';
             $this->render();
 	}
---
+++ application/views/_cell.php
@@ -0,0 +1,11 @@
+<?php
+/*
+ <a href="./data/Katmai_Crater_1980.jpg" data-lightbox="gallery" 
+         data-title="Katmai Crater - Mount Katmai, Alaska ... Posted 2014.05.05 by donald, in landscape">
+         <img src="./data/thumb/Katmai_Crater_1980.jpg"/></a>
+          */              
+?>
+ <a href="./data/{filename}" data-lightbox="gallery" 
+         data-title="{title} ... Posted {uploaded} by {uploader}, in {category}">
+         <img src="./data/thumb/{filename}"/>
+ </a>
